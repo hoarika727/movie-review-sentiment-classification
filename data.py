4a9084afd6637078ea2c47a9f828605667b4e2a1
AK Ho <kho5@hawk.iit.edu>
2020-12-07 22:06:05 -0600

    Add sagemaker models

--- data.py
+++ data.py
@@ -1,12 +1,43 @@
 
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
+from tqdm import tqdm
+import re
+import csv
 
 imdb = keras.datasets.imdb
 
 # load the data
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=100000)
+word_index = imdb.get_word_index(path="imdb_word_index.json")
+
+# prepare the data for other models
+word_index = {k:(v+3) for k,v in word_index.items()} 
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2  # unknown
+word_index["<UNUSED>"] = 3
+reverse_word_index = dict([(v, k) for (k, v) in word_index.items()])
+
+def decode(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+for i in tqdm(range(len(train_data))):
+    train_data[i] = decode(train_data[i])
+    test_data[i] = decode(test_data[i])
+
+train_data = [re.sub('<START> ','',review) for review in train_data]
+test_data = [re.sub('<START> ','',review) for review in test_data]
+
+train = pd.DataFrame({'reviews':train_data, 'labels':train_labels})
+test = pd.DataFrame({'reviews':test_data, 'labels':test_labels})
+train.to_csv('train.csv')
+test.to_csv('test.csv')
+
+train = pd.read_csv('train.csv', index_col='index')
+test = pd.read_csv('test.csv', index_col='index')
 
 '''
 explore the data
@@ -25,6 +56,8 @@ mean = total/len(train_data)     #239
 import matplotlib.pyplot as plt
 plt.hist(word_length)            #250 the most
 '''
+
+# Build a baseline TensorFlow CNN model
 # Transform the data into the same length 
 for i in range(len(train_data)):
     if len(train_data[i]) < 250:
